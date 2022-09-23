a69994be4d907846c80e6940119ec184bc563ee3
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2022-09-23 11:58:13 +0530

    Update Generics_Program.cs

--- Generics_Program.cs
+++ Generics_Program.cs
@@ -1,3 +1,4 @@
+//Coded by: Bethina Dileep
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,11 +26,22 @@ namespace Internal_2
             }
         }
     }
+    class GFG
+    {
+        public void Display<TypeOfValue>(string msg, TypeOfValue value)
+        {
+            Console.WriteLine("{0}:{1}",msg,value);
+        }
+    }
     class Program { 
         static void Main(string[] args)
         {
+            GFG obj = new GFG();
+            obj.Display<int>("Jim", 12);
+            obj.Display<string>("Java", "Programming");
+            obj.Display<double>("C#",2.1);
             Program1<String> name = new Program1<String>();
-            name.value = "DSA";
+            name.value = "Tom";
 
             Program1<int> d = new Program1<int>();
             d.value = 12;

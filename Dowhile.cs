53248e195b108ef332ab0edcd4c9ecca327a374f
Dileep Bethina <56349269+Bethinadileep@users.noreply.github.com>
2023-12-23 15:18:20 +0530

    Update Dowhile.cs

--- Dowhile.cs
+++ Dowhile.cs
@@ -46,3 +46,22 @@ namespace LoopsInC_
         }
     }
 }
+Types of Loops
+1)While Loops
+2)Do while loop
+3)for loop
+4)Foreach loop
+5)Nested for loop
+6)Break 
+7)Continue
+8)Switch
+
+
+Gods word
+
+Jesus Told love who hate you and dishonour you. pray for them in jesus name
+Jechariah 9 : 12
+Return to your fortress, you prisoners of hope even now i announce that I will restore twice as much to you.
+
+    https://www.youtube.com/watch?v=yMOjBupmP2A
+https://www.youtube.com/shorts/NZwnjfLFw2E

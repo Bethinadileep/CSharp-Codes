13e1c7046409f37bf8a99a38f8542c8ab3def896
Dileep Bethina <56349269+Bethinadileep@users.noreply.github.com>
2023-12-23 14:36:54 +0530

    Create WhileLoop.cs

---
+++ WhileLoop.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace LoopsInC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 15;
+            while(i > 0)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+            Console.ReadKey();
+        }
+    }
+}
